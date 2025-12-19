d90da871657fa9828ca4d8689a8d075c24195f8a
nsulonen <niko.sulonen@proton.me>
2025-12-19 13:10:41 +0200

    implement drawLogic and updating

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,6 @@
 const flask_server = 'http://127.0.0.1:5000/';
+const canvas = document.getElementById('vivarium-canvas');
+const context = canvas.getContext('2d');
 
 fetch(flask_server)
   .then(response => {
@@ -10,3 +12,38 @@ fetch(flask_server)
   .catch(error => {
     console.error("Error fetching data:", error);
   });
+
+function drawGrid(grid) {
+  const rowCount = grid.length;
+  if (rowCount === 0) return;
+
+  const colCount = grid[0].length;
+  const cellSize = canvas.width / colCount;
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let r = 0; r < rowCount; r++) {
+    for (let c = 0; c < colCount; c++) {
+
+      if (grid[r][c] === 1) {
+        context.fillStyle = '#222';
+        context.fillRect(
+          c * cellSize,
+          r * cellSize,
+          cellSize,
+          cellSize
+        );
+      }
+    }
+  }
+}
+
+function update() {
+  fetch(flask_server)
+    .then(response => response.json())
+    .then(gridData => {
+      drawGrid(gridData);
+    })
+}
+
+setInterval(update, 100);
