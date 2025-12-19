18d1853f9328ca2141426addc7151005d2041e44
nsulonen <niko.sulonen@proton.me>
2025-12-19 12:50:06 +0200

    add timed lood and threading for simulation

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import random
+import random, time, threading
 
 app = Flask(__name__)
 CORS(app)
@@ -8,11 +8,17 @@ CORS(app)
 width = 10
 height = 10
 
-initial_grid = [[random.choice([0, 1]) for _ in range(width)] for _ in range(height)]
+world_grid = [[random.choice([0, 1]) for _ in range(width)] for _ in range(height)]
 
 @app.route("/")
-def hello_world():
-    return jsonify(initial_grid)
+def get_grid():
+    return jsonify(world_grid)
+
+def run_simulation():
+    global world_grid
+    while True:
+        world_grid = tick(world_grid)
+        time.sleep(1)
 
 def tick(current_grid):
     height = len(current_grid)
@@ -21,11 +27,11 @@ def tick(current_grid):
             
     for row in range(height):
         for col in range(width):
-            live_neighbours = 0
 
+            live_neighbours = 0
             for n_row in range(row - 1, row + 2):
                 for n_col in range(col - 1, col + 2):
-                    if n_row < 0 or n_row >= height or
+                    if n_row < 0 or n_row >= height or \
                        n_col < 0 or n_col >= width:
                         continue
                     if n_row == row and n_col == col:
@@ -34,9 +40,15 @@ def tick(current_grid):
                         live_neighbours += 1
 
             state = current_grid[row][col]
-            if (state == 1 and live_neighbours in [2, 3]) or
+            if (state == 1 and live_neighbours in [2, 3]) or \
                (state == 0 and live_neighbours == 3):
                 new_grid[row][col] = 1                    
 
+    return new_grid
+
 if __name__ == '__main__':
+    simulation_thread = threading.Thread(target=run_simulation)
+    simulation_thread.daemon = True
+    simulation_thread.start()
+    
     app.run(debug=True)
