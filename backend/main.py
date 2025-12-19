17e8f8ac904ba333ed444a22192f403bd95250c6
nsulonen <niko.sulonen@proton.me>
2025-12-19 19:00:46 +0200

    move update logic into simulation class method

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,6 @@ import random, time, threading
 app = Flask(__name__)
 CORS(app)
 
-width = 100
-height = 100
-
-world_grid = [[random.choice([0, 1]) for _ in range(width)] for _ in range(height)]
-
 @app.route("/")
 def get_grid():
     return jsonify(world_grid)
@@ -19,32 +14,6 @@ def run_simulation():
     while True:
         world_grid = tick(world_grid)
         time.sleep(0.1)
-
-def tick(current_grid):
-    height = len(current_grid)
-    width = len(current_grid[0])
-    new_grid = [[0 for _ in range(width)] for _ in range(height)]
-            
-    for row in range(height):
-        for col in range(width):
-
-            live_neighbours = 0
-            for n_row in range(row - 1, row + 2):
-                for n_col in range(col - 1, col + 2):
-                    if n_row < 0 or n_row >= height or \
-                       n_col < 0 or n_col >= width:
-                        continue
-                    if n_row == row and n_col == col:
-                        continue
-                    elif current_grid[n_row][n_col] == 1:
-                        live_neighbours += 1
-
-            state = current_grid[row][col]
-            if (state == 1 and live_neighbours in [2, 3]) or \
-               (state == 0 and live_neighbours == 3):
-                new_grid[row][col] = 1                    
-
-    return new_grid
 
 if __name__ == '__main__':
     simulation_thread = threading.Thread(target=run_simulation)
